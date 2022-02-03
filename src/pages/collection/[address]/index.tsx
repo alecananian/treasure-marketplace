8ddd767a794e9be55342822023ca9279364e649d
Alec Ananian <1013230+alecananian@users.noreply.github.com>
2022-02-03 13:47:58 -0800

    remove console log

--- src/pages/collection/[address]/index.tsx
+++ src/pages/collection/[address]/index.tsx
@@ -478,8 +478,6 @@ const Collection = () => {
     }
   );
 
-  console.log(metadataData);
-
   const { data: legionMetadataData } = useQuery(
     ["metadata-legions", listingData?.pages.length],
     () =>
